c41691e2b19636e850076de36df51f8addf289d8
Ivan Aulia Rahman <ivanar@starva.io>
2023-05-11 19:33:13 +0700

    implement contact us component

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,6 +8,7 @@ import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
 import Newsletter from '@/components/newsletter'
+import ContactUs from '@/components/contact-us'
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
       <Features />
       <FeaturesBlocks />
       <Testimonials />
-      <Newsletter />
+      <ContactUs />
     </>
   )
 }
