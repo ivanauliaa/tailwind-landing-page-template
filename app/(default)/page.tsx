c77c677152122f4244d73e3e9bbd7e59669cbdf7
Ivan Aulia Rahman <ivanar@starva.io>
2023-05-14 10:00:02 +0700

    change Home page header to Home - RumahApp

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Simple',
-  description: 'Page description',
+  title: 'Home - RumahApp',
+  description: 'Home - RumahApp',
 }
 
 import Hero from '@/components/hero'
